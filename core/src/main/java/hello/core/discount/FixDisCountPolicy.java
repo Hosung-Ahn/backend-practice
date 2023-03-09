dfbc4130b0d18691bcd7e2c8e9c02cff120d9636
Hosung <an3735297@naver.com>
2023-03-09 18:32:03 +0900

    excute and test Order, Discount

--- core/src/main/java/hello/core/discount/FixDisCountPolicy.java
+++ core/src/main/java/hello/core/discount/FixDisCountPolicy.java
@@ -3,7 +3,7 @@ package hello.core.discount;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 
-public class FIxDisCountPolicy implements DiscountPolicy{
+public class FixDisCountPolicy implements DiscountPolicy{
 
     private int discountFixAmount = 1000;
     @Override
--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,7 +1,7 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FIxDisCountPolicy;
+import hello.core.discount.FixDisCountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +9,7 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FIxDisCountPolicy();
+    private final DiscountPolicy discountPolicy = new FixDisCountPolicy();
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findNyId(memberId);
