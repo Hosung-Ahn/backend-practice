d2a0e03e15117483100f112b4b0ad991b3fb7785
Hosung <an3735297@pusan.ac.kr>
2023-03-12 14:00:39 +0900

    spring bean / lookup test  / same types more than 1

---
+++ core/src/test/java/hello/core/beanFind/ApplicationContextSameBeanFindTest.java
@@ -0,0 +1,60 @@
+package hello.core.beanFind;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemoryMemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ApplicationContextSameBeanFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SameBeanConfig.class);
+
+    @Test
+    @DisplayName("타입으로 조회 시 같은 타입이 둘 이상 있으면 오류가 발생한다.")
+    void findBeanByTypeDuplicate() {
+        Assertions.assertThrows(NoUniqueBeanDefinitionException.class, () -> {
+            MemberRepository bean = ac.getBean(MemberRepository.class);
+        });
+    }
+    
+    @Test
+    @DisplayName("조회 시 같은 타입이 둘 이상 있으면 이름을 지정하면 된다.")
+    void findBeanByName() {
+        MemberRepository memberRepository1 = ac.getBean("memberRepository1", MemberRepository.class);
+        MemberRepository memberRepository2 = ac.getBean("memberRepository2", MemberRepository.class);
+        assertThat(memberRepository1).isInstanceOf(MemberRepository.class);
+        assertThat(memberRepository2).isInstanceOf(MemberRepository.class);
+    }
+
+    @Test
+    @DisplayName("같은 타입을 가지는 모든 빈 조회하기")
+    void findAllBeanByType() {
+        Map<String, MemberRepository> memberRepositoryMap = ac.getBeansOfType(MemberRepository.class);
+        for (String s : memberRepositoryMap.keySet()) {
+            System.out.println(s + " = " + memberRepositoryMap.get(s));
+        }
+    }
+
+    @Configuration
+    static class SameBeanConfig {
+        @Bean
+        MemberRepository memberRepository1() {
+            return new MemoryMemberRepository();
+        }
+        @Bean
+        MemberRepository memberRepository2() {
+            return new MemoryMemberRepository();
+        }
+    }
+}
