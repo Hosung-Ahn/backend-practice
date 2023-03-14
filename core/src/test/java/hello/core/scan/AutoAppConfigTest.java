2fe002c175d1c779d0bf79df2cef81282b4de30d
Hosung <an3735297@pusan.ac.kr>
2023-03-14 16:36:27 +0900

    ComponentScan / component scan and auto DI

---
+++ core/src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -0,0 +1,32 @@
+package hello.core.scan;
+
+import hello.core.AutoAppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class AutoAppConfigTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+    @Test
+    @DisplayName("모든 빈 조회")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            System.out.println("beanDefinitionName = " + beanDefinitionName);
+            System.out.println("ac.getBean(beanDefinitionName) = " + ac.getBean(beanDefinitionName));
+        }
+    }
+
+    @Test
+    void basicScan() {
+        MemberService memberService = ac.getBean("memberServiceImpl", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
