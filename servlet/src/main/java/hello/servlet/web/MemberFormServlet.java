7029eb5b73492862dc7f40e79819c138a98a2229
Hosung <an3735297@pusan.ac.kr>
2023-03-24 17:08:17 +0900

    basic / create member management app using servlet

---
+++ servlet/src/main/java/hello/servlet/web/MemberFormServlet.java
@@ -0,0 +1,39 @@
+package hello.servlet.web;
+
+import hello.servlet.domain.member.MemberRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "memberFormServlet", urlPatterns = "/servlet/members/new-form")
+public class MemberFormServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+
+        PrintWriter writer = response.getWriter();
+        writer.write("<!DOCTYPE html>");
+        writer.write("<html>");
+        writer.write("  <head>");
+        writer.write("    <meta charset=\"UTF-8\">");
+        writer.write("    <title>Title</title>");
+        writer.write("  </head>");
+        writer.write("  <body>");
+        writer.write("    <form action=\"/servlet/members/save\" method=\"post\">");
+        writer.write("      username: <input type=\"text\" name=\"username\" />");
+        writer.write("      age: <input type=\"text\" name=\"age\" />");
+        writer.write("      <button type=\"submit\">Save</button>");
+        writer.write("    </form>");
+        writer.write("  </body>");
+        writer.write("</html>");
+    }
+}
---
+++ servlet/src/main/java/hello/servlet/web/MemberSaveServlet.java
@@ -0,0 +1,46 @@
+package hello.servlet.web;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "memberSaveServlet", urlPatterns = "/servlet/members/save")
+public class MemberSaveServlet extends HttpServlet {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("MemberSaveServlet.service");
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+        PrintWriter writer = response.getWriter();
+        writer.write("<html>");
+        writer.write("  <head>");
+        writer.write("    <meta charset=\"UTF-8\">");
+        writer.write("    <title>Title</title>");
+        writer.write("  </head>");
+        writer.write("  <body>");
+        writer.write("<ul>");
+        writer.write("    <li>username=" + username + "</li>");
+        writer.write("    <li>age=" + age + "</li>");
+        writer.write("</ul>");
+        writer.write("    <div>저장이 완료되었습니다.</div>");
+        writer.write("    <a href=\"/index.html\">메인</a>");
+        writer.write("  </body>");
+        writer.write("</html>");
+
+    }
+}
