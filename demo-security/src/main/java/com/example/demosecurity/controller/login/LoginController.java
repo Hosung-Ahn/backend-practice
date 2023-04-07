e004e2d071001100786b5dbdcb6a5816a91648ea
Hosung <an3735297@pusan.ac.kr>
2023-04-07 19:03:18 +0900

    security toy / logout controller
    logout 버튼을 누를 경우 실행되는 controller 를 구현하였습니다.

--- demo-security/src/main/java/com/example/demosecurity/controller/login/LoginController.java
+++ demo-security/src/main/java/com/example/demosecurity/controller/login/LoginController.java
@@ -1,12 +1,33 @@
 package com.example.demosecurity.controller.login;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginController {
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        // logout 을 한다는건 이미 인증이 되어있는 상태이기 때문에
+        // SecurityContext 에서 Authentication 을 가져와서 로그아웃 처리를 해준다.
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+
+        return "redirect:/";
+
+        return "/";
     }
 }
