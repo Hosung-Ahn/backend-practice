05c4aebe06bdf97871a018382e1304b4adc5c7c5
Hosung <an3735297@pusan.ac.kr>
2023-04-08 21:50:34 +0900

    security toy / feat CustomAuthenticationHandler
    로그인 성공 이후 이전 url 로 이동할 수 있도록 구현하였습니다.

--- demo-security/src/main/java/com/example/demosecurity/security/handler/CustomAuthenticationHandler.java
+++ demo-security/src/main/java/com/example/demosecurity/security/handler/CustomAuthenticationHandler.java
@@ -1,16 +1,41 @@
 package com.example.demosecurity.security.handler;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
 public class CustomAuthenticationHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private RequestCache requestCache = new HttpSessionRequestCache();
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        super.onAuthenticationSuccess(request, response, authentication);
+
+        setDefaultTargetUrl("/");
+        // 이전 요청 정보를 가져온다.
+        SavedRequest savedRequest = requestCache.getRequest(request, response);
+
+        // 로그인 성공 후 이전 페이지로 이동
+        if (savedRequest != null) {
+            String lastUrl = savedRequest.getRedirectUrl();
+            redirectStrategy.sendRedirect(request, response, lastUrl);
+        } else {
+            redirectStrategy.sendRedirect(request, response, getDefaultTargetUrl());
+        }
     }
+
+
+
 }
