ce535ca9ed722ac7a125dbe3cafabf042ed65633
Hosung <an3735297@pusan.ac.kr>
2023-04-10 00:28:54 +0900

    security toy / fix AjaxAuthenticaitonToken
    권한 설정을 받지 않는 오류를 수정하였습니다.

--- demo-security/src/main/java/com/example/demosecurity/security/token/AjaxAuthenticationToken.java
+++ demo-security/src/main/java/com/example/demosecurity/security/token/AjaxAuthenticationToken.java
@@ -1,33 +1,49 @@
 package com.example.demosecurity.security.token;
 
 import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.util.Assert;
+
+import java.util.Collection;
+import java.util.Collections;
 
 public class AjaxAuthenticationToken extends AbstractAuthenticationToken {
 
-        private String principal;
-        private String credentials;
+    private final Object principal;
 
-        public AjaxAuthenticationToken(String principal, String credentials) {
-            super(null);
-            this.principal = principal;
-            this.credentials = credentials;
-            setAuthenticated(false);
-        }
+    private Object credentials;
 
-        public AjaxAuthenticationToken(Object principal, Object credentials, String secretKey) {
-            super(null);
-            this.principal = principal.toString();
-            this.credentials = credentials.toString();
-            setAuthenticated(false);
-        }
+    /**
+     * This constructor can be safely used by any code that wishes to create a
+     * <code>UsernamePasswordAuthenticationToken</code>, as the {@link #isAuthenticated()}
+     * will return <code>false</code>.
+     *
+     */
+    public AjaxAuthenticationToken(Object principal, Object credentials) {
+        super(null);
+        this.principal = principal;
+        this.credentials = credentials;
+        setAuthenticated(false);
+    }
 
-        @Override
-        public Object getCredentials() {
-            return credentials;
-        }
+    public AjaxAuthenticationToken(Object principal, Object credentials,
+                                               Collection<? extends GrantedAuthority> authorities) {
+        super(authorities);
+        this.principal = principal;
+        this.credentials = credentials;
+        super.setAuthenticated(true); // must use super, as we override
+    }
 
-        @Override
-        public Object getPrincipal() {
-            return principal;
-        }
+
+    @Override
+    public Object getCredentials() {
+        return this.credentials;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return this.principal;
+    }
+
 }
