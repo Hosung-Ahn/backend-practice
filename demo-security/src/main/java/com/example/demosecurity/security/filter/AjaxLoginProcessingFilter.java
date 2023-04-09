db6016e6049694d74f95e32c9f27cc5395d0a71a
Hosung <an3735297@pusan.ac.kr>
2023-04-09 20:03:51 +0900

    security toy / feat AjaxAuthenticationFilter
    api를 사용하기 위해 비동기 인증처리를 구현하는 AjaxAuthenticationFilter 를 만들었습니다.

---
+++ demo-security/src/main/java/com/example/demosecurity/security/filter/AjaxLoginProcessingFilter.java
@@ -0,0 +1,47 @@
+package com.example.demosecurity.security.filter;
+
+import antlr.StringUtils;
+import com.example.demosecurity.domain.AccountDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+public class AjaxLoginProcessingFilter extends AbstractAuthenticationProcessingFilter {
+    public AjaxLoginProcessingFilter() {
+        super(new AntPathRequestMatcher("/api/login"));
+    }
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+        if (!isAjax(request)) {
+            log.error("Authentication is not supported");
+            throw new IllegalStateException("Authentication is not supported");
+        }
+
+        AccountDto accountDto = objectMapper.readValue(request.getReader(), AccountDto.class);
+        if (accountDto.getUsername().isEmpty() || accountDto.getPassword().isEmpty()) {
+            log.error("Username or Password is empty");
+            throw new IllegalArgumentException("Username or Password is empty");
+        }
+
+        return null;
+    }
+
+    private boolean isAjax(HttpServletRequest request) {
+        if ("XMLHttpRequest".equals(request.getHeader("X-Requested-With"))) {
+            return true;
+        }
+        return false;
+    }
+}
