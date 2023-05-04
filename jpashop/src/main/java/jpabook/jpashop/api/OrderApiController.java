ab66d54cad9e9c455d6dee8f4fc501b0fb9671c2
Hosung <an3735297@pusan.ac.kr>
2023-05-04 19:40:48 +0900

    jpa : get orders / findAllByDto_flat
    1:n 을 join 하여 한번에 가져왔기 때문에 같은 order가 여러개 나오게 된다.

--- jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -6,6 +6,7 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.query.OrderFlatDto;
 import jpabook.jpashop.repository.query.OrderQueryDto;
 import jpabook.jpashop.repository.query.OrderQueryRepository;
 import lombok.Data;
@@ -84,6 +85,11 @@ public class OrderApiController {
     @GetMapping("api/v5/orders")
     public List<OrderQueryDto> ordersV5() {
         return orderQueryRepository.findAllByDto_Optimization();
+    }
+
+    @GetMapping("api/v6/orders")
+    public List<OrderFlatDto> ordersV6() {
+        return orderQueryRepository.findAllByDto_flat();
     }
 
 
--- jpashop/src/main/java/jpabook/jpashop/repository/query/OrderQueryRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/query/OrderQueryRepository.java
@@ -63,4 +63,15 @@ public class OrderQueryRepository {
         orders.forEach(o -> o.setOrderItems(orderItemMap.get(o.getOrderId())));
         return orders;
     }
+
+    public List<OrderFlatDto> findAllByDto_flat() {
+        return em.createQuery(
+                "select new jpabook.jpashop.repository.query.OrderFlatDto(o.id, m.name, o.orderDate, o.status, d.address, i.name, oi.orderPrice, oi.count)" +
+                        " from Order o" +
+                        " join o.member m" +
+                        " join o.delivery d" +
+                        " join o.orderItems oi" +
+                        " join oi.item i", OrderFlatDto.class)
+                .getResultList();
+    }
 }
