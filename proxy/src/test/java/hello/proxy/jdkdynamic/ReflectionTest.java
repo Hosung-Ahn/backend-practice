950e9d1f591e53888e70bbd51e149eceded14e6d
Hosung <an3735297@naver.com>
2023-10-20 22:14:52 +0900

    동적 프록시 : 리플렉션

---
+++ proxy/src/test/java/hello/proxy/jdkdynamic/ReflectionTest.java
@@ -0,0 +1,79 @@
+package hello.proxy.jdkdynamic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+@Slf4j
+public class ReflectionTest {
+
+    @Test
+    void reflection0() {
+        Hello target = new Hello();
+        // 공통 로직 1 시작
+        log.info("start");
+        String result1 = target.callA();
+        log.info("result={}", result1);
+        // 공통 로직 1 종료
+
+        // 공통 로직 2 시작
+        log.info("start");
+        String result2 = target.callB();
+        log.info("result={}", result2);
+        // 공통 로직 2 종료
+    }
+
+    @Test
+    void reflection1() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        // 클래스 정보
+        Hello target = new Hello();
+
+        // callA method 정보
+        Class<?> classHello = Class.forName("hello.proxy.jdkdynamic.ReflectionTest$Hello");
+        Method callA = classHello.getMethod("callA");
+        Object result1 = callA.invoke(target);
+        log.info("result1={}", result1);
+
+        // callA method 정보
+        Method callB = classHello.getMethod("callB");
+        Object result2 = callB.invoke(target);
+        log.info("result2={}", result2);
+    }
+
+    @Test
+    void reflection2() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        // 클래스 정보
+        Hello target = new Hello();
+
+        // callA method 정보
+        Class<?> classHello = Class.forName("hello.proxy.jdkdynamic.ReflectionTest$Hello");
+        Method callA = classHello.getMethod("callA");
+        dynamicCall(callA, target);
+
+        // callA method 정보
+        Method callB = classHello.getMethod("callB");
+        dynamicCall(callB, target);
+    }
+
+
+
+    private void dynamicCall(Method method, Object object) throws InvocationTargetException, IllegalAccessException {
+        log.info("start");
+        Object result = method.invoke(object);
+        log.info("result={}", result);
+    }
+
+    @Slf4j
+    static class Hello {
+        public String callA() {
+            log.info("callA");
+            return "A";
+        }
+        public String callB() {
+            log.info("callB");
+            return "B";
+        }
+    }
+}
